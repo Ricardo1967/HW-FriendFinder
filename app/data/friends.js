81f5361e649e6bc7d5a0668b649ad1663b4ce6ad
Ricardo <bibi.montero@gmail.com>
2018-08-21 19:24:21 -0400

    final Code

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friends = [
+    {
+        name: "Bugs Bunny",
+        photo: "https://i.ebayimg.com/images/g/Q9gAAOSwmgJY29lJ/s-l300.jpg",
+        scores: [
+          "5",
+          "1",
+          "5",
+          "5",
+          "4",
+          "1",
+          "5",
+          "1",
+          "1",
+          "1"
+        ]
+      },
+
+      {
+        name: "Elmer Fudd",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/6/66/ElmerFudd.gif/220px-ElmerFudd.gif",
+        scores: [
+          "1",
+          "3",
+          "1",
+          "3",
+          "1",
+          "3",
+          "3",
+          "5",
+          "1",
+          "5"
+        ]
+      },
+
+      {
+        name: "Daffy Duck",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e4/Daffy_Duck_2011.svg/145px-Daffy_Duck_2011.svg.png",
+        scores: [
+          "1",
+          "5",
+          "1",
+          "1",
+          "5",
+          "5",
+          "1",
+          "1",
+          "5",
+          "1"
+        ]
+      },
+
+
+
+];
+
+module.exports = friends;
